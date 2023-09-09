5cda0f193e4bb73ba3cebf2bbfc2e30f28e0091e
dhirajpatil001 <125031214+dhirajpatil001@users.noreply.github.com>
2023-09-09 22:58:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+	if(num==1 || num == 2){ return num-1 }
+	
+	let a = 0;
+	let b = 1;
+    let c;
+	for(let i = 3 ; i <= num;i++){
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return c;
 }
 
 module.exports = fibonacci;
